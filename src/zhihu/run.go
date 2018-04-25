223361516a7770828e72921e54c066ae5e8c4c5f
gao8954 <895407214gao@sina.com>
2018-04-25 20:27:31 +0800

    add run

---
+++ src/zhihu/run.go
@@ -0,0 +1,11 @@
+package main
+
+import "zhihu/core"
+
+func main() {
+	booster := new(core.XBooster)
+	booster.ModelPath = "./resources/models/model_20180412_Pos8.bin"
+	booster.LoadModel()
+	booster.Predict()
+
+}
